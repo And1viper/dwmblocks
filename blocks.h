cf7cc32923cc097d05a70bd739fe324bad4f731b
And1viper <daminov.arslan@gmial.com>
2020-11-23 15:11:02 +0000

    statuscmd patch & blocks added

--- blocks.h
+++ blocks.h
@@ -1,8 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "~/bin/statusbar/volume",						0,		10},
-
+	{"", "volume",						0,		10},
+	{"", "brightness",                                          0,              13},	
+	{"", "battery",                                          5,              2},
+	{"", "clock",                                          60,             1},
+	{"", "internet",                                          5,             3},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
